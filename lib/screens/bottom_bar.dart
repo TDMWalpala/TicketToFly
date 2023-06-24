26dbbf2068c792a7c2d13ee319f55a18b65e22e2
TDMWalpala <tharindudarshana2019@gmail.com>
2023-06-24 09:50:52 +0530

    init

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Tickets"),
+      ),
+      body: Center(child: Text("my body")),
+    );
+  }
+}
